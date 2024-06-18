171a09fc2f2a060e56a41861c58735cbb5ee3db0
Adisak-KS <Adisak.General@gmail.com>
2024-06-18 17:43:32 +0700

    React Variable

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const updateCounter = () => {
+    setCounter(counter + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        hello React.js
+    <div className="">
+      <header className="">
+        counter = {counter}
       </header>
+      <button onClick={updateCounter}>Click Here for update Value of counter</button>
     </div>
   );
 }
