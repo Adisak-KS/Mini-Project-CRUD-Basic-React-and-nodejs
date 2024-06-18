c6273e0fbf5378ccc7a79f11fc6920e19c190e6e
Adisak-KS <Adisak.General@gmail.com>
2024-06-18 11:59:27 +0700

    status code

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.json()); 
-app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Hello World555!')
@@ -21,12 +21,29 @@ app.put('/hello/myPut/:id', (req, res) => {
     res.send('id = ' + req.params.id + ' name = ' + req.body.name);
 });
 
-app.delete('/hello/myDelete/:id', (req, res)=>{
+app.delete('/hello/myDelete/:id', (req, res) => {
     res.send('id = ' + req.params.id)
 })
 
-app.post('/hello/postFormJason', (req, res)=>{
+app.post('/hello/postFormJason', (req, res) => {
     res.send(req.body.name + " " + req.body.phone);
+})
+
+app.post('/login', (req, res) => {
+    try {
+        const username = req.body.username;
+        const password = req.body.password;
+
+        if (username == 'Admin' && password == 'Admin') {
+            res.send({ message: "success" });
+        }
+        else{
+            res.status(401).send({ message: 'unauthorized' });
+        }
+
+    } catch (e) {
+        res.status(500).send({ message: e.message });
+    }
 })
 
 
