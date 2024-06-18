f48c9feabf5d968ce8f60b62ac4743041e2f2b29
Adisak-KS <Adisak.General@gmail.com>
2024-06-18 20:38:08 +0700

    React useEffect

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,17 @@
+import { useEffect } from "react";
+
 function App() {
   const padding = '10px';
   const color = 'red';
+
+  useEffect(()=>{
+    fetchData()
+  },[]);
+
+  const fetchData = () =>{
+    console.log('page on load')
+  }
+
   return (
     <div style={{ marginTop: "50px", marginLeft:"50px"}}>
      <input style={{borderColor: color, padding: padding}} />
