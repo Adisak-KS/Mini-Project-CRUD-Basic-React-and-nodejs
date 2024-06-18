3dfdc72005d45efcd4dcff7626d09bb06d692b34
Adisak-KS <Adisak.General@gmail.com>
2024-06-18 11:17:44 +0700

    Method post, put, delete

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+// app.use(express.json()); 
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.send('Hello World555!')
+})
+
+app.get('/hello/:name', (req, res) => {
+    res.send("Hello My Name is " + req.params.name)
+})
+
+app.post('/hello/myPost', (req, res) => {
+    res.send('Hello ' + req.body.name);
+})
+
+app.put('/hello/myPut/:id', (req, res) => {
+    res.send('id = ' + req.params.id + ' name = ' + req.body.name);
+});
+
+app.delete('/hello/myDelete/:id', (req, res)=>{
+    res.send('id = ' + req.params.id)
+})
+
+// app.post('/hello/postFormJason', (req, res)=>{
+//     res.send(req.body);
+// })
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
