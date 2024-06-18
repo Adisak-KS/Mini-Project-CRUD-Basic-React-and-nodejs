00e6d7b043f04d823cce59c487dab9423c970406
Adisak-KS <Adisak.General@gmail.com>
2024-06-18 14:08:15 +0700

    Send object

--- server.js
+++ server.js
@@ -14,6 +14,16 @@ app.delete('/hello/myDelete/:id', (req, res) => controller.myDelete(req, res));
 app.post('/hello/postFormJason', (req, res) => controller.postFormJason(req, res));
 app.post('/login', (req, res) => controller.login(req, res));
 
+app.get('/myObject', (req, res) =>{
+    const user = {
+        name: 'Adisak',
+        age: 22,
+        status: true,
+        salary: 9000.85
+    }
+
+    res.send({result:user})
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
